be1369558385c560e8f03703fb180436076dcd02
Marek Včelák <vcelak.marek@gmail.com>
2024-01-25 20:43:12 +0100

    add default paths to config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
+const root = resolve(__dirname, "src");
+
+// prettier-ignore
 export default defineConfig({
   base: "/Interview_Project/",
   plugins: [react()],
   root: "src",
+  resolve: {
+    alias: {
+      "types": resolve(root, "types"),
+      "components": resolve(root, "components"),
+      "context": resolve(root, "context"),
+      "api": resolve(root, "api")
+    },
+  },
 });
