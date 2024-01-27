0d285240b863f29a91432630ab7a1941e9350a30
Marek Včelák <vcelak.marek@gmail.com>
2024-01-27 20:06:43 +0100

    add repos and orgs variants

--- src/connectors/fetch.ts
+++ src/connectors/fetch.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-export const GET = async <T>(userName: string): Promise<T> => {
-  const { data } = await axios.get(`https://api.github.com/users/${userName}`);
+export const GET = async <T>(
+  userName: string,
+  repos = false,
+  orgs = false
+): Promise<T> => {
+  const { data } = await axios.get(
+    `https://api.github.com/users/${userName}${
+      repos ? "/repos" : orgs ? "/orgs" : ""
+    }`
+  );
   console.log(data);
   return data;
 };
