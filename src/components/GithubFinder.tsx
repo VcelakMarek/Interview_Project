71c5c2e0a9886d8b1065db3ecfb6cbbcdce8abd5
Marek Včelák <vcelak.marek@gmail.com>
2024-01-27 21:57:15 +0100

    add UserDetails

--- src/components/GithubFinder.tsx
+++ src/components/GithubFinder.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material";
 import SearchBar from "components/SearchBar";
 import UserInfo from "components/UserInfo";
+import UserDetails from "./UserDetails";
 
 const GithubFinder = () => {
   return (
     <Container>
       <SearchBar />
       <UserInfo />
+      <UserDetails />
     </Container>
   );
 };
