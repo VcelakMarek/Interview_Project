35f467b2635caf11f4c8fac21d957c611b583333
VcelakMarek <vcelak.marek@gmail.com>
2024-01-29 12:27:12 +0100

    fix: correct data access

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,47 +1,45 @@
-import { useContext } from "react";
-import { UserDataContext } from "context/userData.context";
+import useUserDataContext from "components/useUserDataContext";
 import { Box, Card, List, ListItem, ListItemText, Link } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 const UserInfo = () => {
-  const { userData } = useContext(UserDataContext);
-
-  console.log("userData:", userData);
+  const { userData } = useUserDataContext();
+  const { userInfo } = userData;
 
   return (
     <Box>
       <Card sx={{ width: 345 }}>
         <CardMedia
           sx={{ height: 345, width: 345 }}
-          image={userData.avatar_url}
-          title={`${userData.name}_profile_picture`}
+          image={userInfo?.avatar_url}
+          title={`${userInfo?.name}_profile_picture`}
           component="img"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {userData.name}
+            {userInfo?.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {userData.bio}
+            {userInfo?.bio}
           </Typography>
           <List>
             <ListItem disablePadding>
-              <ListItemText primary={userData.company} />
+              <ListItemText primary={userInfo?.company} />
             </ListItem>
 
             <ListItem disablePadding>
-              <ListItemText primary={userData.location} />
+              <ListItemText primary={userInfo?.location} />
             </ListItem>
 
             <ListItem disablePadding>
-              <ListItemText primary={userData.email} />
+              <ListItemText primary={userInfo?.email} />
             </ListItem>
 
             <ListItem disablePadding>
-              <Link href={userData.blog} color="inherit">
-                {userData.blog}
+              <Link href={userInfo?.blog} color="inherit">
+                {userInfo?.blog}
               </Link>
             </ListItem>
           </List>
